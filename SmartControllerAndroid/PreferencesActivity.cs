36be66e203caed3852767c453f20f6355bb1fb09
Atria <atriasft@gmail.com>
2021-02-08 03:37:56 +0900

    設定画面の原型完成

---
+++ SmartControllerAndroid/PreferencesActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace SmartControllerAndroid
+{
+    [Activity(Label = "PreferencesActivity")]
+    public class PreferencesActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_preferences);
+
+            if (savedInstanceState == null)
+            {
+                var fragment = new PreferencesFragment();
+                var transaction = SupportFragmentManager.BeginTransaction();
+                transaction.Replace(Resource.Id.fragment_container, fragment);
+                transaction.Commit();
+            }
+        }
+    }
+}
